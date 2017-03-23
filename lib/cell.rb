5fbb7188c8caa5dd9647147e5263d0c629c03b2b
Beth Sebian <esebian@gmail.com>
2017-03-22 21:50:38 -0600

    Refactor cell

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Cell
-  attr_reader :neighbors, :condition, :touched
+  attr_reader :condition, :neighbors, :touched
 
   def initialize(condition = "dead")
     @condition = condition
@@ -11,28 +11,28 @@ class Cell
   end
 
   def transform
-    touch_and_transform_all
+    set_next_condition_all
     untouch_all
     update_condition_all
     untouch_all
   end
 
-  def touch_and_transform_all
-    count = @neighbors.count do |position, neighbor|
-      neighbor.condition == "alive"
-    end
+  def set_next_condition_all
+    @touched = true
+    count = neighbors.count {|position, neighbor| neighbor.condition == "alive"}
     @next_condition = "dead" if count < 2 || count > 3 #
     @next_condition = "alive" if count == 3
-    @touched = true
-    untouched = neighbors.values.select {|neighbor| neighbor.touched == false}
-    untouched.each {|neighbor| neighbor.touch_and_transform_all}
+    untouched_cells.each {|neighbor| neighbor.set_next_condition_all}
   end
 
   def update_condition_all
     @touched = true
     @condition = @next_condition
-    untouched = neighbors.values.select {|neighbor| neighbor.touched == false}
-    untouched.each {|neighbor| neighbor.update_condition_all}
+    untouched_cells.each {|neighbor| neighbor.update_condition_all}
+  end
+
+  def untouched_cells
+    neighbors.values.select {|neighbor| neighbor.touched == false}
   end
 
   def untouch_all
